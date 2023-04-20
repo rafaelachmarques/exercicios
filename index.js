79f1a7a83f86fc47ab886dec5390844e5386241f
Aluno talentos <rafael.rodrigues022@hotmail.com>
2023-04-19 21:35:33 -0300

    subindo exercícios de variáveis

---
+++ index.js
@@ -0,0 +1,6 @@
+// Exercício 1 O que será impresso é 10,10 e 5 pois são os valores que estão no console
+// Exercício 2 O que será impresso é 10,10 e 10 pois são os valores que estão no console
+// Exercício 3 
+let horasTrabalhadas = prompt("quantas horas voce trabalha por dia?")
+let dinheiroRecebido = prompt("quanto voce recebe por dia?")
+alert(`voce recebe ${horasTrabalhadas/dinheiroRecebido}por hora`)
