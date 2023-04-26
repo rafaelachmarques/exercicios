19f9e3c6daf6e79bfc890fe380ee393de83c68c3
Aluno talentos <rafael.rodrigues022@hotmail.com>
2023-04-25 21:49:27 -0300

    exercício

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
-// Exercício 1 O que será impresso é 10,10 e 5 pois são os valores que estão no console
-// Exercício 2 O que será impresso é 10,10 e 10 pois são os valores que estão no console
-// Exercício 3 
-let horasTrabalhadas = prompt("quantas horas voce trabalha por dia?")
-let dinheiroRecebido = prompt("quanto voce recebe por dia?")
-alert(`voce recebe ${horasTrabalhadas/dinheiroRecebido}por hora`)
+// // Exercício 1 O que será impresso é 10,10 e 5 pois são os valores que estão no console
+// // Exercício 2 O que será impresso é 10,10 e 10 pois são os valores que estão no console
+// // Exercício 3 
+// let horasTrabalhadas = prompt("quantas horas voce trabalha por dia?")
+// let dinheiroRecebido = prompt("quanto voce recebe por dia?")
+// alert(`voce recebe ${horasTrabalhadas/dinheiroRecebido}por hora`)
+// Exercício 1. 1 
+const primeiroNumero = 3
+const segundoNumero = 4
+const resultado = primeiroNumero + segundoNumero
+console.log(resultado) = 7
+// Exercíco 1. 2
+const primeiroNumero = 3
+const segundoNumero = 5
+const terceiroNumero = 2
+const resultado = primeiroNumero * segundoNumero / terceiroNumero
+console.log(resultado) // 7,5
+// Exercício 1.3
+const primeiroNumero = 5
+const segundoNumero = 4
+const terceiroNumero = -1
+const resultado = (primeiroValor - segundoValor) * terceiroValor
+console.log(resultado)// 1
+// Exercício 1.4
+const resultado = 234 % 5
+console.log(resultado) = 4
+// Exercício 2
+console.log(primeiroNumero === segundoNumero);
+console.log(primeiroNumero!== segundoNumero);
+console.log(primeiroNumero>segundoNumero);
+console.log(primeiroNumero>=segundoNumero);
+console.log(primeiroNumero<segundoNumero);
+console.log(primeiroNumero<=segundoNumero)
+// Exercício 5
+const nome = prompt("Digite seu nome")
+const idade= Number("Digite sua idade")
+const maiorDeIdade = idade > 19
+const idadeEm2050 = 2050 - 2023
+console.log(new Date().getFullYear());
+console.log(idadeEm2050);
+console.log("nome",nome);
+console.log("É maior de idade", maiorDeIdade);
+console.log("Idade em 2050", idadeEm2050)
+
+
+
